47ec4453098b59226f4547559db4badf031e24be
ElleP <143535896+ElliotRoosevelt@users.noreply.github.com>
2025-03-21 21:13:09 -0400

    Add files via upload

---
+++ Data/How to Obtain Initial Datasets.R
@@ -0,0 +1,7 @@
+
+#Initial DS was too big to fit in Github. 
+##Access Google Drive for inital ds
+#1. Enter Scripts Google Drive folder or https://drive.google.com/drive/folders/1Y5bCtBVGn-IfO6hbN7QwIKEQhk-E9BRk?usp=sharing 
+
+#Link to access this doc: https://docs.google.com/document/d/11dfSyXrN98hCoXwbiK3upI0QSgGmP19Af2Ry0ZpBvOg/edit?usp=sharing 
+
